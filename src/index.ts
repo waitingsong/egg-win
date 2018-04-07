24df01bf8a8a4206d5a39ef5b95f5487e17580b2
waiting <1661926154@qq.com>
2018-04-07 19:53:31 +0800

    feat: hide worker console windows and set title

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export * from './lib/index'
+import * as egg from 'egg'
+
+import Agent from './lib/agent'
+import Application from './lib/application'
+import startCluster from './lib/cluster'
+
+const newEgg = {
+  ...egg,
+  Agent,
+  Application,
+  startCluster,
+}
+export = newEgg
---
+++ src/lib/cluster.ts
@@ -0,0 +1,59 @@
+import { startCluster, ClusterOptions } from 'egg'
+import * as nwwc from 'windows-window-controller'
+
+
+export interface ClusterOptionsInt extends ClusterOptions {
+  framework: string
+  baseDir: string
+  plugins: object | null
+  workers: number
+  port: number
+  https: boolean
+  key: string
+  cert: string
+  clusterPort: number
+  title?: string
+}
+
+export interface ClusterMsg {
+  action: 'egg-ready' | 'egg-pids'
+  to: 'agent' | 'app'
+  data?: ClusterOptionsInt | number[] | any
+  from: 'agent' | 'app' | 'master'
+}
+
+// change window title
+process.on('message', (msg: ClusterMsg) => {
+  if (msg && msg.action) {
+    const { action, data, from, to } = msg
+    const titleNew = parseKeyStr(data.title ? data.title : data.baseDir)
+
+    // console.info('::message:', process.pid, process.ppid, msg)
+    if (action === 'egg-ready') {
+      if ((to === 'app' || to === 'agent') && from === 'master') {
+        titleNew && titleNew !== process.title && nwwc.set_title(titleNew, {
+          matchType: 'pid',
+          matchValue: process.pid,
+        })
+        .then(() => {
+          nwwc.hide({
+            matchType: 'pid',
+            matchValue: process.pid,
+          })
+        })
+        .catch(err => { })
+      }
+    }
+  }
+})
+
+function parseKeyStr(str: string): string {
+  const regexDim = /\\+/g
+  const ret = str && typeof str === 'string' ? str : ''
+  return ret.replace(regexDim, '/')
+}
+
+export default startCluster
+// export default (options: ClusterOptions, cb: (...args: any[]) => any) => {
+//   return startCluster(options, cb)
+// }
